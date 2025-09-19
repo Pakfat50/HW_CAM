97fba65afccc9bb4733e85808e316221b13ac10b
Pakfat50 <ialove127@gmail.com>
2025-09-19 15:42:34 +0900

    自己交差の交点を追加する処理を行うと点列が乱れるため、交点を追加する処理をコメントアウト

--- src/cam_generic_lib.py
+++ src/cam_generic_lib.py
@@ -623,9 +623,9 @@ def remove_self_collision(x, y):
             p4 = [x[j+1], y[j+1]]
             if cross_judge(p1, p2, p3, p4) == True:
                 detection = True
-                cx, cy = getCrossPointFromPoint(p1[0], p1[1], p3[0], p3[1], p2[0], p2[1], p4[0], p4[1])
-                new_x.append(cx)
-                new_y.append(cy)             
+                #cx, cy = getCrossPointFromPoint(p1[0], p1[1], p3[0], p3[1], p2[0], p2[1], p4[0], p4[1])
+                #new_x.append(cx)
+                #new_y.append(cy)             
                 i = j+1
             j += 1
         new_x.append(x[i])
